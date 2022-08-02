67d4812bff8265ee7811a7b31a69a8672e32299c
João Bittencourt <j.v_dias@hotmail.com>
2022-08-02 16:56:14 -0300

    feat: margin config and aditional css

--- config/pdf-manager.php
+++ config/pdf-manager.php
@@ -1,6 +1,15 @@
 <?php
 
 return [
+    /**
+     * Margin in CM
+     */
+    'margin' => [
+        'top'    => 3,
+        'right'  => 1.5,
+        'left'   => 1.5,
+        'bottom' => 3,
+    ],
     'footer' => [
         'css' => '
                 left: 0px;
@@ -19,4 +28,5 @@ return [
             text-align: center;
         ',
     ],
+    'aditional_css' => null
 ];
--- resources/views/defaultLayout.blade.php
+++ resources/views/defaultLayout.blade.php
@@ -2,10 +2,10 @@
 <head>
     <style>
         @page {
-            margin: 3cm 1.5cm 3cm;
-        }
-        .table-sm {
-            font-size: 10pt !important;
+            margin: {{ config('pdf-manager.margin.top', 3) }}cm
+                    {{ config('pdf-manager.margin.right', 1.5) }}cm
+                    {{ config('pdf-manager.margin.bottom', 3) }}cm
+                    {{ config('pdf-manager.margin.left', 1.5) }}cm;
         }
         #header {
             position: fixed;
@@ -18,28 +18,18 @@
         .page_break {
             page-break-before: always;
         }
-        table {
-            border-collapse: collapse !important;
-        }
-        table thead th {
-            text-align: left;
-        }
-        table.table td,
-        th {
-            border: 0.4pt solid black;
-            padding: 1px 2px 1px 2px;
-        }
+        {{ config('pdf-manager.aditional_css') }}
     </style>
 </head>
 <body>
 @if (!empty($header))
     <div id="header">
-        {{ $header }}
+        {!! $header !!}
     </div>
 @endif
 @if (!empty($footer))
     <div id="footer">
-        {{ $footer }}
+        {!! $footer !!}
     </div>
 @endif
 {!! $structure !!}
